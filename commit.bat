12468e222bdbd65497923be0f988a6493c0c1e02
Kelvin Arthur <Kelvin@labdept.local>
2025-09-10 21:49:22 +0000

    feat: Enhanced validation and UX improvements

--- commit.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-git add .
-git commit -m "feat: Enhanced validation and UX improvements"
-git push origin main_refactor
--- commit.ps1
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "feat: Enhanced validation and UX improvements"
-git push origin main_refactor
